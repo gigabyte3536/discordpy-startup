5b0e5fe589e10d6e7189fd4f692fc240dc3df728
gigabyte3536 <48615033+gigabyte3536@users.noreply.github.com>
2020-09-28 21:59:43 +0900

    Update kanibot.py

--- kanibot.py
+++ kanibot.py
@@ -12,32 +12,12 @@ TOKEN = os.environ["TOKEN"]
 async def on_ready():
     print("稼働の準備が完了しました")
     
-@client.event
-async def message_count(channel):
-    matched = re.match(r'(.+)（(\d+)）',channel.name)
-    if matched:
-        #もし、メッセージのカウントが既に行われていたら
-        base_name = matched.groups()[0] # 元のチャンネル名
-        count = int(matched.groups()[1]) # メッセージ数
-        await channel.edit(name=f"{base_name}（{count+1}）")
-        return
-
-    count = 0
-    async for message in channel.history(limit=None):
-        if not message.author.bot:
-            # 人間が投稿していたらカウントを1増やす
-            count += 1 
-
-    await channel.edit(name=f"{channel.name}（{count}）")
 
 @client.event
-
 async def on_message(message):
-
- if message.author.bot:
+     if message.author.bot:
         return
-        
- await message_count(message.channel)
- print("投稿完了")
+    emoji = client.get_emoji(310177266011340803)
+    await message.add_reaction(emoji)
 
 client.run(TOKEN)
